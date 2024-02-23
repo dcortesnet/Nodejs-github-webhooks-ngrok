6be0d55b9abc19ec7d286dbe77c46222584d8933
dcoretsnet <dcortes.net@gmail.com>
2024-02-23 16:14:55 -0300

    Updates

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 let events = [];
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.post("/hook", (req, res) => {
+  console.log("Capture event of Github WebHook");
+  events.push(req.body);
+  return res.status(200).end();
+});
+
+app.get("/events", (req, res) => {
+  return res.json(events).status(200);
 });
 
 app.listen(port, () => {
